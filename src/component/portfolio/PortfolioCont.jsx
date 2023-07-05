e6534cc9571375ca0c1a4c541f7a054ff18da624
testAnGyonam <skadldldl123@gmail.com>
2023-07-05 16:00:21 +0900

    2023.07.05

--- src/component/portfolio/PortfolioCont.jsx
+++ src/component/portfolio/PortfolioCont.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import WebStandard from "../../assets/images/port/webstandard.png";
 
-
 const PortfolioCont = () => {
   return (
     <div className="port__box">
       <ul>
         <li>
-          <a href="#">
+          <a href="https://xlsak5.github.io/testAnGyonam/">
             <img src={WebStandard} alt="만든 사이트1" />
 
             <div className="port__desc">
               <span>웹표준 사이트입니다.</span>
-              <p>6개월 동안 배웠던 것들과 만든 것들을 모아 놓은 사이트입니다.</p>
+              <p>
+                6개월 동안 배웠던 것들과 만든 것들을 모아 놓은 사이트입니다.
+              </p>
             </div>
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="https://cloneeee.netlify.app/">
             <img src={WebStandard} alt="만든 사이트1" />
 
             <div className="port__desc">
@@ -27,7 +28,7 @@ const PortfolioCont = () => {
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="https://an-vue01.netlify.app/">
             <img src={WebStandard} alt="만든 사이트1" />
 
             <div className="port__desc">
@@ -37,28 +38,32 @@ const PortfolioCont = () => {
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="http://xlsak3.dothome.co.kr/">
             <img src={WebStandard} alt="만든 사이트1" />
 
             <div className="port__desc">
               <span>PHP로 제작한 팀프로젝트 입니다.</span>
-              <p>6개월 동안 배웠던 것들과 만든 것들을 모아 놓은 사이트입니다.</p>
+              <p>
+                6개월 동안 배웠던 것들과 만든 것들을 모아 놓은 사이트입니다.
+              </p>
             </div>
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="https://tetris-portfolio-d4291.web.app/">
             <img src={WebStandard} alt="만든 사이트1" />
 
             <div className="port__desc">
               <span>JAVASCRIPT로 제작한 게임 입니다.</span>
-              <p>6개월 동안 배웠던 것들과 만든 것들을 모아 놓은 사이트입니다.</p>
+              <p>
+                6개월 동안 배웠던 것들과 만든 것들을 모아 놓은 사이트입니다.
+              </p>
             </div>
           </a>
         </li>
       </ul>
     </div>
   );
-}
+};
 
 export default PortfolioCont;
